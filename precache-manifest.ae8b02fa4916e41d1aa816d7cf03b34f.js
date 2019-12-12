1beb4243ba3091480d038184814c528828ae8b9d
Megan Akiba <megan.akiba@colorado.edu>
2019-12-11 19:04:31 -0700

    Updates

---
+++ precache-manifest.ae8b02fa4916e41d1aa816d7cf03b34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9532d6b1f7b2ad678f8707b990ca1afc",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "7cbe23b4952d6f622d5d",
+    "url": "/tictactoe/static/css/main.b8edd830.chunk.css"
+  },
+  {
+    "revision": "f5c4018ea2135e72dc64",
+    "url": "/tictactoe/static/js/2.30376982.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tictactoe/static/js/2.30376982.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7cbe23b4952d6f622d5d",
+    "url": "/tictactoe/static/js/main.b2eabab8.chunk.js"
+  },
+  {
+    "revision": "474cf4d7a83a31de1701",
+    "url": "/tictactoe/static/js/runtime-main.6e72561c.js"
+  }
+]);
